baccf30187a004d94445a65147f9273d692082d0
Amirat Abdulganiyu <leaderswordga@gmail.com>
2025-12-25 16:20:15 +0100

    learn the importance of using the box-sizing, it helped make sure the box did't have an extra box outside.In the javascript i made it possible to put two different value for for the row and the columns

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 const newDiv = document.querySelector(".container");
 console.log(newDiv);
+const gridRow = document.querySelector("#row");
+const gridCol = document.querySelector("#col");
+const btn = document.querySelector("#createGrid");
 
-for (let i = 0; i < 16; i++) {
-  const grids = document.createElement("div");
-  grids.className = "grids";
-  newDiv.appendChild(grids);
-}
+const makeAGrid = function () {
+  newDiv.innerHTML = "";
+  let rightSide = Number(gridCol.value);
+  let topSide = Number(gridRow.value);
+  let totalSide = topSide * rightSide;
+  for (let i = 0; i < totalSide; i++) {
+    const grid = document.createElement("div");
+    grid.className = "grids";
+
+    grid.style.width = `calc(100% / ${rightSide})`;
+    console.log((grid.style.width = `calc(100% / ${rightSide})`));
+
+    grid.style.height = `calc(100% / ${topSide})`;
+    newDiv.appendChild(grid);
+  }
+  console.log(totalSide);
+};
+newDiv.addEventListener("mouseover", function (e) {
+  // console.log(e, "hover");
+  if (e.target.classList.contains("grids")) {
+    e.target.style.backgroundColor = "black";
+  }
+});
+// makeAGrid(2, 4);
+btn.addEventListener("click", makeAGrid);
